6969f35d1c6cb1da90def9836992f7b900b0c1e1
Hieu <hieu.dcu@gmail.com>
2025-04-04 13:49:07 +1300

    Update

---
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,117 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _db.Products.ToList();
+            return View(objProductList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index");
+            }
+            if (!ModelState.IsValid)
+            {
+                TempData["error"] = "Failed to create";
+            }
+                return View();
+        }
+
+
+        public string GetAllCategories()
+        {
+            return "Return all categories";
+        }
+
+        public string GetCategoriesByNames(string name)
+        {
+            return $"Return All Categories With NAmes: {name}";
+        }
+
+        // Edit
+      
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id==0)
+            {
+                return NotFound();
+            }
+            Product? productFromDb = _db.Products.Find(id);
+
+            if (productFromDb==null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Product edited successfully";
+                return RedirectToAction("Index");
+            }
+            else if (!ModelState.IsValid)
+            {
+                TempData["error"] = "Failed to edit product";
+            }
+                return View();
+        }
+
+        // Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? productFromDb = _db.Products.Find(id);
+
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Product? obj = _db.Products.Find(id);
+            if (obj==null)
+            {
+                return NotFound();
+            }
+            _db.Products.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Delete successful!";
+            return RedirectToAction("Index");
+        }
+    }
+}
