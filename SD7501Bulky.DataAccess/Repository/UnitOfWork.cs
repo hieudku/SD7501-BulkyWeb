e0de27200ef1c7cb81a7859c6514c4269755bf0c
Huba <hieu.dcu@gmail.com>
2025-05-22 23:12:49 +1200

    Update: UnitOfWork

---
+++ SD7501Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using SD7501Bulky.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SD7501Bulky.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _db;
+        public ICategoryRepository Category { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
